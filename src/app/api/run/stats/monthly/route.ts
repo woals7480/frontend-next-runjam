006a9672b9581b7ea21c4c24b0b1c4e2e8d45356
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-10-10 02:47:55 +0900

    수정

--- src/app/api/run/stats/monthly/route.ts
+++ src/app/api/run/stats/monthly/route.ts
@@ -1,14 +1,12 @@
 // app/api/run/stats/monthly/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic"; // ✅ 쿠키/헤더를 고려해 항상 동적 처리
-export const revalidate = 0; // ✅ 캐시 방지 (보강)
-
 export async function GET(req: NextRequest) {
   const API = process.env.NEXT_PUBLIC_API_URL!;
   const AT = process.env.COOKIE_NAME_AT!;
   const url = `${API}/run/stats/monthly`;
   const at = req.cookies.get(AT)?.value;
+  console.log(at);
   if (!at) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
